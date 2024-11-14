7260ca4adc1b190ae05be62623ecabf44c9c6f50
ToyB-Chan <jasjason0otto@gmail.com>
2024-11-14 01:29:27 +0100

    Update dllmain.cpp
    Made the payload try to detect and skip exception loops

--- Payload/dllmain.cpp
+++ Payload/dllmain.cpp
@@ -1,9 +1,26 @@
 #include <windows.h>
 
+#define HISTORY_SIZE 1024
+
+DWORD64 exceptionRipHistory[HISTORY_SIZE];
+DWORD64 historyIndex = 0;
+
 LONG WINAPI MyFilter(PEXCEPTION_POINTERS pExceptionInfo)
 {
-    pExceptionInfo->ContextRecord->Rip++; 
+	DWORD64 instructionsToSkip = 1; // The instruction that brought us here to begin with
+	for (DWORD64 i = 0; i < sizeof(HISTORY_SIZE); ++i)
+	{
+		if (exceptionRipHistory[i] == pExceptionInfo->ContextRecord->Rip)
+		{
+			instructionsToSkip++; // We've already encountered a crash of this instruction just very recently, lets try to skip more in case we are in some kind of exception loop
+			break;
+		}
+	}
 
+	exceptionRipHistory[historyIndex] = pExceptionInfo->ContextRecord->Rip;
+	historyIndex = (historyIndex + 1) / HISTORY_SIZE
+	
+    pExceptionInfo->ContextRecord->Rip += instructionsToSkip;
     return EXCEPTION_CONTINUE_EXECUTION;
 }
 
