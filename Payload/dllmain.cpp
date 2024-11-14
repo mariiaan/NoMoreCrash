41b1e6853c36f57968d9af9a1608d7d6a419bd1f
mariiaan <marian@aomori.dev>
2024-11-14 09:51:31 +0100

    Fix some stuff

--- Payload/dllmain.cpp
+++ Payload/dllmain.cpp
@@ -5,33 +5,28 @@
 DWORD64 exceptionRipHistory[HISTORY_SIZE];
 DWORD64 historyIndex = 0;
 
-LONG WINAPI MyFilter(PEXCEPTION_POINTERS pExceptionInfo)
+static LONG WINAPI MyFilter(PEXCEPTION_POINTERS pExceptionInfo)
 {
-	DWORD64 instructionsToSkip = 1; // The instruction that brought us here to begin with
-	for (DWORD64 i = 0; i < sizeof(HISTORY_SIZE); ++i)
+	DWORD64 numBytesToSkip = 1; // The instruction that brought us here to begin with
+	for (DWORD64 i = 0; i < HISTORY_SIZE; ++i)
 	{
 		if (exceptionRipHistory[i] == pExceptionInfo->ContextRecord->Rip)
-		{
-			instructionsToSkip++; // We've already encountered a crash of this instruction just very recently, lets try to skip more in case we are in some kind of exception loop
-			break;
-		}
+			++numBytesToSkip; // We've already encountered a crash of this instruction just very recently, lets try to skip more in case we are in some kind of exception loop
 	}
 
 	exceptionRipHistory[historyIndex] = pExceptionInfo->ContextRecord->Rip;
-	historyIndex = (historyIndex + 1) / HISTORY_SIZE;
+	if (++historyIndex == HISTORY_SIZE)
+		historyIndex = 0;
 	
-    pExceptionInfo->ContextRecord->Rip += instructionsToSkip;
-    return EXCEPTION_CONTINUE_EXECUTION;
+	pExceptionInfo->ContextRecord->Rip += numBytesToSkip;
+	return EXCEPTION_CONTINUE_EXECUTION;
 }
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+BOOL APIENTRY DllMain(HMODULE, DWORD, LPVOID)
 {
-    // just do it every time, don't care.
+	// just do it every time, don't care.
 	SetUnhandledExceptionFilter(MyFilter);
 
-    return TRUE;
+	return TRUE;
 }
 
